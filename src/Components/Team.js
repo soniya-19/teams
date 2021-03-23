992ed090c49b44bc7abccb1d9b8a072fa7029b43
Soniya-19 <hi.kapoors@gmail.com>
2021-03-23 16:58:30 -0400

    code

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -13,30 +13,35 @@ const Team = ({team}) => {
     }, [])
     //fetch user
     const fetchUser = async()=>{
-        const res= await fetch('https://cgjresszgg.execute-api.eu-west-1.amazonaws.com/user/')
+        const res= await fetch('https://cgjresszgg.execute-api.eu-west-1.amazonaws.com/users/')
         const data = await res.json()
         return data
     }
     return (
         <div className='task'>
-            <button onClick={(Event)=>{
+             <button onClick={(Event)=>{
                 filterUser(team.id)
                 }
-            }>{team.name}</button>
-            {users.map((user)=>(
-        <User key={user.id} user={user} />
-        ))} 
-           
+            } className='task'>{team.name}</button>
+            <div id="divUser" style={{display:'none'}}>
+                 {users.map((user)=>(
+                    <User key={user.id} user={user} /> 
+            ))}   
+        </div>
         </div>
     )
-    function filterUser(val){
+     function filterUser(val){
+         document.getElementById("divUser").style.display='block';
+         
         const filterUserName = _.filter({users}, function (data) {
-            console.log(data)
-            return data.id == val;
+         
+            return data[0].teamId[0] == val;
         });
-        const filterUserlist = filterUserName.map(function(filterUserName) {
-            return filterUserlist;
+        console.log(filterUserName)
+        //map the filtered dat on UI
+        const newEmojipedia = filterUserName.map(function(filterUserName) {
+        return filterUserName
         });
-    }
+    } 
 }
 export default Team
